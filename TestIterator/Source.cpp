6da15c4a5da846b50fad44e9708dbd31c19a908e
Kamar <kmprojects1998@gmail.com>
2021-04-04 18:10:43 +0300

    Add more classes of iterator

--- TestIterator/Source.cpp
+++ TestIterator/Source.cpp
@@ -1,35 +1,53 @@
 ﻿#include <iostream>
 #include "Queue.h"
 #include "IIterator.h"
+#include "Person.h";
+#include <string>
 
 using namespace std;
 
-bool predicate(int number)
-{
-	return number > 5;
-}
+
+
 
 bool isEven(int number)
 {
 	return number % 2 == 0;
 }
 
+Person convert(int age)
+{
+	Person p(age, "Jack");
+	return p;
+}
+
 int main(int argc, char* args[])
 {
 	Queue<int> queue;
-	for (int i = 0; i <= 10; i++)
+	for (int i = 0; i <= 20; i++)
 	{
 		queue.Enqueue(i);
 	}
 
-	IIterator<int>* defIter = queue.getIterator();
-	IIterator<int>* iteratorPredicate = new IterateByPredicate<int>(defIter, predicate);
-	IIterator<int>* iteratorEvenAndBiggerFive = new IterateByPredicate<int>(iteratorPredicate, isEven);
-	while (iteratorEvenAndBiggerFive->moveNext())
+	
+
+	IIterator<int>* queueIterator = queue.getIterator();
+	IIterator<int>* where = new Where<int>(queueIterator, isEven);
+	IIterator<int>* skip = new Skip<int>(where, 3);
+	IIterator<int>* take = new Take<int>(skip, 4);
+
+	IIterator<Person>* select = new Select<int, Person>(take, convert);
+
+	while (select->moveNext())
 	{
-		cout << iteratorEvenAndBiggerFive->getCurrent() << endl;
+		cout << "Name: " << select->getCurrent().getName() << "   Age: " <<
+			select->getCurrent().getAge() << endl;
 	}
 
-	delete iteratorPredicate;
+	delete queueIterator;
+	delete where;
+	delete skip;
+	delete take;
+
+
 	return 0;
 }
